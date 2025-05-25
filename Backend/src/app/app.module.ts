66a1971b9e23124650946f9795f4e2ff62f6b453
Agnes Tsannang Sandjon Voffo <agnes.tsvoffo@student.uni-siegen.de>
2025-05-25 22:38:57 +0200

    Update app.module.ts

--- Backend/src/app/app.module.ts
+++ Backend/src/app/app.module.ts
@@ -5,14 +5,11 @@ import { AppService } from './app.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AuthModule } from '../auth/auth.module';
 import { UsersModule } from '../users/users.module';
-<<<<<<< HEAD
 import { SqlRunnerModule } from '../sql-runner/sql-runner.module';
 import { DatabaseModule } from '../prisma/prisma.module'; // Importiert das DatabaseModule, um auf die Datenbank zuzugreifen
 import { UbungModule } from 'src/ubung/ubung.module';
-=======
 import { TasksModule } from '../tasks/tasks.module';
 import { UploadModule } from '../upload/upload.module';
->>>>>>> 467a4094375b376f6c3bdb9ee07b9e195aa7ea5a
 
 @Module({
   imports: [
@@ -22,14 +19,8 @@ import { UploadModule } from '../upload/upload.module';
     PrismaModule,
     AuthModule,
     UsersModule,
-<<<<<<< HEAD
-    SqlRunnerModule,
-    UbungModule,
-    DatabaseModule,
-=======
     TasksModule,
     UploadModule,
->>>>>>> 467a4094375b376f6c3bdb9ee07b9e195aa7ea5a
   ],
   controllers: [AppController],
   providers: [AppService],  
